b7d98016201209ad18e51c1ead1fc27f84dc7443
Ji Yim <gsooyim@gmail.com>
2018-04-18 21:25:35 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,17 @@
-# Add  code here!
+def prime?(number)
+  i = 2
+  status = false
+  
+  while i < (number/2)
+    if number <= 1
+      status = false
+    elsif number <= 3
+      status = true
+    elsif number % i !=0
+      status = true
+    else 
+      status = false
+    end
+  end
+  status
+end
