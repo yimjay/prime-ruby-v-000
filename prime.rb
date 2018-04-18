7640a6c04aaacbbf312fc1c17f7c44d789e27cdb
Ji Yim <gsooyim@gmail.com>
2018-04-18 21:29:43 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,9 +1,7 @@
 def prime?(number)
-  test_array = (4..(number - 1)).to_a
+  test_array = (2..(number - 1)).to_a
   if number <= 1
     false
-  elsif number <= 3 
-    true
   else 
     test_array.any? { |n| number % n == 0 } ? false : true
   end
